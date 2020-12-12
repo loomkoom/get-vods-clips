49c53dcbf8b065e2337af4059761f21509273f48
loomkoom <29258685+loomkoom@users.noreply.github.com>
2020-12-12 14:39:11 +0100

    fixed typo

--- bin/get_vod.py
+++ bin/get_vod.py
@@ -60,7 +60,7 @@ def get_vod(channel_name, broadcast_id, timestamp, test = "no"):
              "https://d2e2de1etea730.cloudfront.net",
              "https://ds0h3roq6wcgc.cloudfront.net"]
 
-    urls = [f"{host}{final_formatted_string}/chunked/index-dvr.m3u8" for host in hosts]
+    urls = [f"{host}/{final_formatted_string}/chunked/index-dvr.m3u8" for host in hosts]
     header = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36 OPR/71.0.3770.323', }
 
