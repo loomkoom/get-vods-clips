d1a8634775a6f6dd799d888dd2d112531c4350e1
loomkoom <29258685+loomkoom@users.noreply.github.com>
2020-12-21 11:54:08 +0100

    removed brotli encoding

--- bin/get_stream_data.py
+++ bin/get_stream_data.py
@@ -16,8 +16,8 @@ logger.addHandler(stream_handler)
 
 def get_page(url, output = "html", session = False):
     headers = {
-            'Accept'                   : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'Accept-Encoding'          : 'gzip, deflate, br',
+            'Accept'                   : 'text/html,application/xhtml+xml,application/xml;q=0.9,application/json,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'Accept-Encoding'          : 'gzip, deflate',
             'Accept-Language'          : 'en-US,en;q=0.9,nl;q=0.8',
             'Dnt'                      : '1',
             'Sec-Gpc'                  : '1',
